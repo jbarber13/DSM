536265746a5ab5f1d43aafe32cc0bb9fd8f9aef3
Jake Barber <jabot999@gmail.com>
2021-03-31 15:55:48 -0400

    initial project setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
-import photo from '../photo.png'
+import photo from '../images/photo.png'
 
 class Navbar extends Component {
 
@@ -9,28 +9,37 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://www.jake-barber.com/#barberbucks-swap"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
-          Decentragram
+          &nbsp;Decentralized Social Media (DSM)
         </a>
-        <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id="account">{'0x0'}</small>
-            </small>
-            { this.props.account
-              ? <img
-                className='ml-2'
-                width='30'
-                height='30'
-                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
-              />
-              : <span></span>
-            }
+        
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a
+              className="nav-link small"
+              href={`https://kovan.etherscan.io/address/${this.props.account}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+               Your Account: {this.props.account}
+            </a>
           </li>
+
+          { /**Identicon - only show if account exists*/
+            this.props.account
+            ? <img
+              className="ml-2"
+              width="30"
+              height="30"
+              src={`data:image/png;base64, ${new Identicon(this.props.account, 30).toString()}`}
+              alt=""
+              />
+              :<span></span>
+          }
         </ul>
       </nav>
     );
---
+++ src/contracts/DSM.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LICENSE
+pragma solidity >=0.5.0;
+
+
+contract DSM {
+  // Code goes here...
+}
