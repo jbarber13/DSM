0bc2fdc9b5a7e35d3414ef80dad042368b3c5f3d
Jake <jabot999@gmail.com>
2021-07-20 14:58:37 -0400

    added a line

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 class App extends Component {
+  
 
   //run before render function
   async componentWillMount() {
